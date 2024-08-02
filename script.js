294d38c2b618f4254fcccf1b6dc0a5b7cae6c7dc
Shimoteg <gabrielshimote@outlook.com>
2024-08-02 18:15:03 -0300

    add script and adjust theme dark/light

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+   const html = document.documentElement
+    
+    if (html.classList.contains('light')) {
+       html.classList.remove('light') 
+    } else {
+       html.classList.add('light')
+    }
+}   
+
+// pega o elemento selecionado / CSS / Selector
+const nomeDog = document.querySelector('.text-lulu')
+
+if(html.classList.contains('light')) {
+    
+    nomeDog.setAttribute('')
+}
